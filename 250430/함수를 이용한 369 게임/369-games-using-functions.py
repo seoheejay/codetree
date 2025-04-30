0e3cabc7926a86d63223e100ba57417c7dd0fb26
seoheejay <138648909+seoheejay@users.noreply.github.com>
2025-04-30 22:16:46 +0900

    Title: 함수를 이용한 369 게임, Time: 98ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250430/함수를 이용한 369 게임/369-games-using-functions.py
@@ -0,0 +1,20 @@
+a, b = map(int, input().split())
+
+# Please write your code here.
+def fun(a,b):
+    cnt=0
+    #arr=[3,6,9]
+    for i in range(a,b+1):
+        if i%3==0:
+            cnt+=1
+        else:
+            i=str(i) 
+            for x in i: #2랑9
+                if x=="3" or x=="6" or x=="9":
+                    cnt+=1
+    print(cnt)
+
+fun(a,b)
+
+
+
