410ab3d25974b9df3e1619d5e2ed67c7918ecca0
seoheejay <138648909+seoheejay@users.noreply.github.com>
2025-05-01 17:13:14 +0900

    Title: 함수를 이용한 소수 판별, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250501/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 250501/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -1,15 +1,23 @@
 a, b = map(int, input().split())
 
 # Please write your code here.
-def fun(a,b):
-    sum=0
-    for i in range(a,b+1):
-        for j in range(2,i):
-            if i%j==0:
-                break
-            if j==i-1:
-                sum+=i
+def fun(a, b):
+    sum = 0
+    for i in range(a, b + 1):
+        if i == 1:
+            continue  # 1은 소수가 아님
+        elif i == 2:
+            sum += 2  # 2는 소수
+        else:
+            is_prime = True  #초기화
+            for j in range(2, i):
+                if i % j == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                sum += i
     print(sum)
+
 
 
 fun(a,b)
