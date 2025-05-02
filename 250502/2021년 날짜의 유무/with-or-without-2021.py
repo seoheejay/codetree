45ec5085f36266415358f504eb5b6831f162d6a0
seoheejay <138648909+seoheejay@users.noreply.github.com>
2025-05-02 19:02:07 +0900

    Title: 2021년 날짜의 유무, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250502/2021년 날짜의 유무/with-or-without-2021.py
+++ 250502/2021년 날짜의 유무/with-or-without-2021.py
@@ -7,18 +7,15 @@ def fun(M,D):
         if M==i:
             #D일 존재하는지 여부
             if M==2: 
-                for j in range(1,29): #28일까지
-                    if D==j:
-                        result=True
-                        break
+                if D<=28:
+                    result=True
+                    break
             elif M in [1,3,5,7,8,10,12]:
-                for j in range(1,32): #31일까지
-                    if D==j:
+                    if D<=31:
                         result=True
                         break
             elif M in [4,6,9,10,11]:
-                for j in range(1,31): #30일까지
-                    if D==j:
+                    if D<=30:
                         result=True
                         break
             break
