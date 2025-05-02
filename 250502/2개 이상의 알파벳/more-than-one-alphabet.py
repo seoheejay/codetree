b4b6d9b96d77f7d46361c7d7b5cd372e806ed23d
seoheejay <138648909+seoheejay@users.noreply.github.com>
2025-05-02 22:22:23 +0900

    Title: 2개 이상의 알파벳, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250502/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 250502/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -2,14 +2,8 @@ A = input()
 
 # Please write your code here.
 def fun(A):
-    arr=list(A)
-    cnt=0
-    x=arr[0] #codetree
-    for j in range(1,len(arr)):
-        if x!=arr[j]:
-            cnt+=1
-            
-    if cnt>=2:
+    arr=set(A)           
+    if len(arr)>=2:
         print("Yes")
     else:
         print("No")
