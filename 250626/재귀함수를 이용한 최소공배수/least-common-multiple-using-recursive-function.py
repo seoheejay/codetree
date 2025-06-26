9222533b4d19eba941737d1a7fa960cb10759e6c
seoheejay <138648909+seoheejay@users.noreply.github.com>
2025-06-26 17:44:35 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 98ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 250626/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 250626/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -1,23 +1,15 @@
+import math
+
 n = int(input())
 arr = list(map(int, input().split()))
-arr.sort() #1,2,5,6,7,9
-lcm = arr[n-1] #이게 가장 큰 수
+arr.sort()
 
-# Please write your code here.
-def f(arr,n,lcm):
-    if n==0:
-        return lcm
-    #elif arr[n-1]%arr[n-2] == 0:
-    #    return f(arr,n-1,lcm)
-    else:
-        for i in range(1,arr[n-2]+1): #작은 수
-            if arr[n-2]%i==0:
-                if lcm%i==0:
-                    continue
-                else:
-                    lcm*=i 
-        return f(arr,n-1,lcm)
+def lcm(a, b):
+    return a * b // math.gcd(a, b)
 
-        
+def f(arr, n):
+    if n == 1:
+        return arr[0]
+    return lcm(arr[n-1], find_lcm(arr, n-1))
 
-print(f(arr,n,lcm))
+print(f(arr, n))
