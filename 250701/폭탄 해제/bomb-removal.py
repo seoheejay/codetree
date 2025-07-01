10d4df0557014d29d39c70aecda817283e83cd93
seoheejay <138648909+seoheejay@users.noreply.github.com>
2025-07-01 11:12:01 +0900

    Title: 폭탄 해제, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250701/폭탄 해제/bomb-removal.py
@@ -0,0 +1,14 @@
+unlock_code, wire_color, seconds = input().split()
+seconds = int(seconds)
+
+# Please write your code here.
+class Struc:
+    def __init__(self, unlock_code, wire_color, seconds):
+        self.unlock_code = unlock_code
+        self.wire_color = wire_color
+        self.seconds = seconds
+
+struc = Struc(unlock_code, wire_color, seconds)
+
+print("")
+print("")
