a8502c4cc461daec261ba5dc90ab6cee13918fb9
seoheejay <138648909+seoheejay@users.noreply.github.com>
2025-07-07 12:13:56 +0900

    Title: 블럭쌓는 명령2, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 250707/블럭쌓는 명령2/block-stacking-commands2.py
+++ 250707/블럭쌓는 명령2/block-stacking-commands2.py
@@ -3,11 +3,10 @@ n, k = map(int, input().split())
 #n개의 칸이 있음
 #명령이 K번 주어짐
 #1부터 n번 칸까지 쌓인 블럭의 수 중 최댓값 출력 > 누적값 
-lst =[0]*n
+lst =[0]*n 
 
-for i in range(k):
+for _ in range(k):
     a,b = map(int,input().split())
     for j in range(a,b+1):
-        lst[j]+=1
-
+        lst[j-1]+=1
 print(max(lst))
