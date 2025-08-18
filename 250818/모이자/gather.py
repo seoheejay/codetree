8c4e8b74b152e6e6b65d23a2b78bd8eb303b4f64
seoheejay <138648909+seoheejay@users.noreply.github.com>
2025-08-18 13:14:59 +0900

    Title: 모이자, Time: 52ms, Memory: 17MB, Status: Passed - Codetree

--- 250818/모이자/gather.py
+++ 250818/모이자/gather.py
@@ -9,10 +9,11 @@ min_val = int_min
 res = 0 
 cnt = 1 #첫 번째 집 ~ n 번째 집
 for _ in range(n): #n번만큼 반복시키고
-    for i in arr:
-        res+=i*(n-i)
+    for i in range(n):
+        res+=arr[i]*abs(i+1-cnt)
     cnt+=1
     min_val=min(min_val, res)
+    res=0
    
 
 print(min_val)
