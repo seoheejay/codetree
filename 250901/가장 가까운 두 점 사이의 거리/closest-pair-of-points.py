ec4d1e36781d849ac4747195aadfd8fdf06dd8a1
seoheejay <138648909+seoheejay@users.noreply.github.com>
2025-09-01 10:12:29 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 73ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 250901/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
@@ -0,0 +1,19 @@
+import sys
+INT_MAX = sys.maxsize
+
+n = int(input())
+points  [tuple(map(int, input().split())) for _ in range(n)]
+
+# 두 점 사이의 거리의 제곱 값 반환
+def dist(i,j):
+    x1,y1 = points[i]
+    x2,y2 = points[j]
+    return (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2)
+
+#최솟값 찾기
+min_dist = INT_MAX
+for i in range(n):
+    for j in range(i+1,n):
+        min_dist = min(min_dist, dist(i,j))
+
+print(min_dist)
