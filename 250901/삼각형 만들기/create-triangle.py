e30246903982e8679df1a3c7ebbb06c4d33fc160
seoheejay <138648909+seoheejay@users.noreply.github.com>
2025-09-01 12:56:06 +0900

    Title: 삼각형 만들기, Time: 124ms, Memory: 20MB, Status: Passed - Codetree

--- 250901/삼각형 만들기/create-triangle.py
+++ 250901/삼각형 만들기/create-triangle.py
@@ -11,10 +11,12 @@ def choose(i,j,k):
     #단, x축과 평행, y축과 평행
     if (x1==x2 or x1==x3 or x2==x3) and (y1==y2 or y2==y3 or y1==y3):
         return(area(x1,y1,x2,y2,x3,y3))
+    else:
+        return 0
     
 #삼각형 넓이에 2 곱한 값
 def area(x1,y1,x2,y2,x3,y3):
-    return abs((x1*y2 + x2*y3 + x3*y1)-(x2*y1+x3*y2+x1*y3)) #신발끈공식
+    return abs((x1*y2 + x2*y3 + x3*y1)-(x2*y1+x3*y2+x1*y3)) #신발끈
 
 #3개 골라 삼각형
 max_area = 0
